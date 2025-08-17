894b2ccb6efcf1065aa4477efef900a294a89f91
clorbh <sleepyzzt@gmail.com>
2025-08-17 19:48:12 -0300

    Delete toyhouse-lite directory

--- toyhouse-lite/src/utils/slug.js
+++
@@ -1,7 +0,0 @@
-export function slugify(str) {
-  return (str || 'item')
-    .normalize('NFD').replace(/[̀-ͯ]/g, '')
-    .toLowerCase()
-    .replace(/[^a-z0-9]+/g,'-')
-    .replace(/(^-|-$)+/g,'');
-}
